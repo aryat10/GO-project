90b9efe147b815784778332198ff201b07db8704
Aryat <aryatsrishadow007@gmail.com>
2025-07-06 22:06:18 +0530

    Refactor study.go by commenting out existing code and adding variable declaration examples within if and switch constructs

--- Study/study.go
+++ Study/study.go
@@ -2,32 +2,51 @@ package main
 
 import "fmt"
 
-func loop() {
-	for i := 1; i <= 10; i++ {
-		fmt.Print(i, " ")
-	}
-}
+// func loop() {
+// 	for i := 1; i <= 10; i++ {
+// 		fmt.Print(i, " ")
+// 	}
+// }
 func main() {
-	loop()
-	var name = "Hello"
-	secName := "World"
+	// loop()
+	// var name = "Hello"
+	// secName := "World"
 
-	fmt.Println("\n", name, secName)
+	// fmt.Println("\n", name, secName)
 
-	var (
-		age = 20
-		sex = "male"
-	)
+	// var (
+	// 	age = 20
+	// 	sex = "male"
+	// )
 
-	fmt.Println(age, sex)
+	// fmt.Println(age, sex)
 
-	var a, b, c int = 1, 2, 3
+	// var a, b, c int = 1, 2, 3
 
-	fmt.Println(a, b, c)
+	// fmt.Println(a, b, c)
 
-	var d = 1
-	var e = 2
-	var f = 3
+	// var d = 1
+	// var e = 2
+	// var f = 3
 
-	fmt.Println(d, e, f)
+	// fmt.Println(d, e, f)
+
+	// we can declare a variable inside if construct
+	// if age := 20; age > 18 {
+	// 	fmt.Println("You are an adult")
+	// } else {
+	// 	fmt.Println("You are a minor")
+	// }
+
+	// we can declare a variable inside switch construct
+	switch age := 20; age {
+	case 18:
+		fmt.Println("You are an adult")
+	case 19:
+		fmt.Println("You are a minor")
+	case 20:
+		fmt.Println("You are an adult")
+	default:
+		fmt.Println("You are a minor")
+	}
 }
